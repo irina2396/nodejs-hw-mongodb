cb000380a4062b909b8a6d8d4e9c2154b85f59bb
Iryna Ponikarchyk <irishka.ponikarchik@ukr.net>
2025-09-18 13:55:06 +0300

    add file crud

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,29 @@
 import { ContactsCollection } from '../db/models/contacts.js';
 
-export const getAllContacts = async () => {
-    const contacts = await ContactsCollection.find();
-    return contacts;
+export function getAllContacts() {
+    return ContactsCollection.find();
 };
 
-export const getContactById = async (contactId) => {
-    const contact = await ContactsCollection.findById(contactId);
-    return contact;
+export function getContactById(contactId) {
+    return ContactsCollection.findById(contactId);
 };
+
+export function createContact(payload) {
+    return ContactsCollection.create(payload);
+};
+
+export async function updateContact(contactId, payload) {
+    const result = await ContactsCollection.findByIdAndUpdate(contactId, payload, {
+        new: true,
+        upsert: true,
+        includeResultMetadata: true,
+    });
+    return {
+        value: result.value,
+        isNew: Boolean(result?.lastErrorObject?.upserted),
+    };
+};
+
+export function deleteContact(contactId) {
+    return ContactsCollection.findOneAndDelete({ _id: contactId });
+}
